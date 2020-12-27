5eba123d0c263daa9b12e8f6c095228a92976c5d
amangupta37 <amangupta6121998@gmail.com>
2020-12-27 14:25:05 +0530

    Added  Writing file Async feature

--- Basics_of_Node/02-Read-Write-[Async]/index.js
+++ Basics_of_Node/02-Read-Write-[Async]/index.js
@@ -5,10 +5,21 @@ const mod= require('fs');
 
 mod.readFile('./txt/start.txt' ,'utf-8' , (err,data) =>{
 
-    mod.readFile(`./txt/${data}.txt` ,'utf-8' , (err,data) =>{
+    mod.readFile(`./txt/${data}.txt` ,'utf-8' , (err,new_data) =>{
 
-        console.log(data);
+        console.log(new_data);
+
+        mod.writeFile('./txt/output.txt',`This the new data which we have to write \n ${new_data}`,'utf-8', err =>{
+
+            console.log("data is written Successfuly...!!!")
+        })
+
+
+
     })
 })
 
 console.log("Reading value......");
+
+//--------Writing data Asynchronously----------
+
