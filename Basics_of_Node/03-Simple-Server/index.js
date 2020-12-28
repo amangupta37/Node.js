176c93604a319d3423386cd96a795011fa2de9f2
Aman Gupta <amangupta6121998@gmail.com>
2020-12-28 15:07:12 +0530

    Update index.js

--- Basics_of_Node/03-Simple-Server/index.js
+++ Basics_of_Node/03-Simple-Server/index.js
@@ -1,18 +1,18 @@
 
 
-//----------------Creating a Simple Server in Nodejs----------------------
+//----------------Creating a Simple Server in Nodejs-------------------
 
-// add a module http for network operation
+// Added a module http for network operation
 
 const http = require ('http');
 
-//Create a Server 
+// Created a Server 
 
 const server = http.createServer((req,res) =>{
     res.end("Hello from Server...!!!")
 })
 
-// starting the server or listining to client request
+// Starting the server or listining to client request
 
 server.listen(5000,'127.0.0.1',() =>{
     console.log('Listining the request at 5000')
