8d871f3ed4889c3799cf586b579ec84aa6daa8a7
amangupta37 <amangupta6121998@gmail.com>
2020-12-28 17:06:20 +0530

    Added http status code (404)

---
+++ Basics_of_Node/05-http_Status_Code/index.js
@@ -0,0 +1,42 @@
+//-------------Adding HTTP STATUS CODE (404 NOT FOUND) in Node.js-------------------
+
+// Added a http modeule
+
+const http = require('http');
+
+// Added a url module 
+
+const url = require('url');
+
+
+// Created a server
+
+const server = http.createServer((req,resp) =>{
+
+    // Implementing Routing 
+    
+    const path = req.url;  // storing the url into a variable
+
+    if(path==='/' || path==='/overview')
+    {
+        resp.end("This is an OVERVIEW...!!!");
+
+    }
+    else if(path==='/product')
+    {
+        resp.end("This is a PRODUCT...!!!");
+    }
+    else
+    {
+        resp.writeHead(404); // Adding http status code for page not found (i.e 404 status code)
+
+        resp.end("Page Not Found");
+    }
+})
+
+// Start The server
+
+server.listen(3000,'127.0.0.1', () =>{
+
+    console.log("Listining to request at port 3000");
+})
