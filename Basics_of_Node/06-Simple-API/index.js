94bdaa92e4f4bdd419ac5e4fd43e468ba9a4e117
amangupta37 <amangupta6121998@gmail.com>
2020-12-30 23:05:45 +0530

    added a index file for demo

--- Basics_of_Node/06-Simple-API/index.js
+++ Basics_of_Node/06-Simple-API/index.js
@@ -1,1 +1,13 @@
 //--------------Creating a simple web API -------------------------
+
+const path = require('path');
+
+//const data = path.dirname('c/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
+//const data1 = path.parse('c:/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
+const data2 = path.parse(`${__dirname}/index.js`);
+
+//console.log(data);
+//console.log(data1);
+console.log(data2)
+
+console.log()
