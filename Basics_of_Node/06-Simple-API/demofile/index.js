9ca79b9abc2736660ecdaed779070918bc7ece73
amangupta37 <amangupta6121998@gmail.com>
2020-12-31 19:53:34 +0530

    Implemented path in nodejs by __dirname

--- Basics_of_Node/06-Simple-API/demofile/index.js
+++ Basics_of_Node/06-Simple-API/demofile/index.js
@@ -1,9 +1,10 @@
-//const data = path.dirname('c/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
-//const data1 = path.parse('c:/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
+const path = require('path');
+
+const data = path.dirname('c/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
+const data1 = path.parse('c:/Users/Aman gupta/Adv_Nodejs/Basics_of_Node/06-Simple-API/index.js')
 const data2 = path.parse(`${__dirname}/index.js`);
 
-//console.log(data);
-//console.log(data1);
+console.log(data);
+console.log(data1);
 console.log(data2)
 
-console.log()
