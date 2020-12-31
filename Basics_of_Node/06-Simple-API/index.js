e8ee0cf65e749261d0843bb37941cb788f232adf
amangupta37 <amangupta6121998@gmail.com>
2020-12-31 20:16:52 +0530

    implemeted routing

--- Basics_of_Node/06-Simple-API/index.js
+++ Basics_of_Node/06-Simple-API/index.js
@@ -1,12 +1,25 @@
 //--------------Creating a simple web API -------------------------
 const http = require('http');
+const url = require('url');
 
 
 //-----------------Create a server------------
 
 const server = http.createServer((req,res) =>{
 
-    res.end("hello world");
+    const pathname = req.url;
+
+   if(pathname==='/' || pathname === '/home')
+   {
+       res.end("Welcome to home");
+   }
+   else if(pathname==='/api')
+   {
+       res.end("Welcome to api")
+   }
+   else{
+       res.end("Page not Available");
+   }
 
 })
 
@@ -14,5 +27,5 @@ const server = http.createServer((req,res) =>{
 
 server.listen(4000,'127.0.0.1' , () =>{
 
-    console.log("Listining to the request at 4000");
+    console.log("Listining to the request at port 4000");
 })
