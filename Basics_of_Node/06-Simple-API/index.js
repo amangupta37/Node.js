6b13c7f9d275ce760308421cf537fcf4ddc5ff06
amangupta37 <amangupta6121998@gmail.com>
2020-12-31 20:08:33 +0530

    created a server at port 4000

--- Basics_of_Node/06-Simple-API/index.js
+++ Basics_of_Node/06-Simple-API/index.js
@@ -1,1 +1,18 @@
 //--------------Creating a simple web API -------------------------
+const http = require('http');
+
+
+//-----------------Create a server------------
+
+const server = http.createServer((req,res) =>{
+
+    res.end("hello world");
+
+})
+
+//-------------listen incomming request--------------
+
+server.listen(4000,'127.0.0.1' , () =>{
+
+    console.log("Listining to the request at 4000");
+})
