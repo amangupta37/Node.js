d8378bf6f9890afb636bc401a632260a99d75cda
amangupta37 <amangupta6121998@gmail.com>
2021-01-03 10:51:26 +0530

    updated dir

---
+++ 01-Basics_of_Node/01-Read-Write/index.js
@@ -0,0 +1,18 @@
+//--------Read file synchronously in node.js---------
+
+const md = require('fs');
+const path = require('path');
+const textread = md.readFileSync('./txt/read-this.txt' , 'utf-8');
+console.log(textread);
+
+
+//---------Writing in file synchronously in node.js  ------
+
+
+const addtext = `performing writing operation in node.js \n This is what we know about avocado : ${textread}`;
+md.writeFileSync('./txt/output.txt',addtext);
+console.log("Data is sucessfully added in file");
+
+
+
+  
