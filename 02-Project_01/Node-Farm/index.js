f11d82219a9a7bc6aa70a6dda37a9023d411b592
amangupta37 <amangupta6121998@gmail.com>
2021-01-09 01:15:35 +0530

    updated index.js

--- 02-Project_01/Node-Farm/index.js
+++ 02-Project_01/Node-Farm/index.js
@@ -12,7 +12,7 @@ const temp_Overview = md.readFileSync(`${__dirname}/templates/template-overview.
 const temp_Card = md.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const newdata = md.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const dataObj = JSON.parse(newdata); // string to json coversion
-//const read_overview = md.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8')
+
 //-----------------Create a server---------------------
 
 function replacetemp (objdata,changedata) {
@@ -24,17 +24,14 @@ function replacetemp (objdata,changedata) {
 
    placedata = placedata.replace(/{%IMAGE%}/g,objdata.image);
    placedata =placedata.replace(/{%PRICE%}/g,objdata.price);
-   placedata =placedata.replace(/%FROM%/g,objdata.from);
-   placedata =placedata.replace(/%NUTRIENTS%/g,objdata.nutrients);
-   placedata =placedata.replace(/%QUANTITY%/g,objdata.quantity);
-   placedata =placedata.replace(/%DESCRIPTIONS%/g,objdata.description);
-   placedata =placedata.replace(/%ID%/g,objdata.id);
-  if(!objdata.organic)
-  {
-  placedata = placedata.replace(/%NOT_ORGANIC%/g,'not-organic')
-  }
-  //console.log(placedata);
-  return placedata;
+   placedata =placedata.replace(/{%FROM%}/g,objdata.from);
+   placedata =placedata.replace(/{%NUTRIENTS%}/g,objdata.nutrients);
+   placedata =placedata.replace(/{%QUANTITY%}/g,objdata.quantity);
+   placedata =placedata.replace(/{%DESCRIPTIONS%}/g,objdata.description);
+   placedata =placedata.replace(/{%ID%}/g,objdata.id);
+   if(!objdata.organic)
+   placedata = placedata.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+   return placedata;
 }
 
 const server = http.createServer((req,res) =>{
@@ -45,7 +42,7 @@ const server = http.createServer((req,res) =>{
    {    
        
       const ovrdata = dataObj.map(el => replacetemp(el,temp_Card))
-      console.log(ovrdata);
+      //console.log(ovrdata);
      
        res.writeHead(200,{'Content-type':'text/html'})
        const finaloutput = temp_Overview.replace(/%PRODUCT_CARD%/,ovrdata)
